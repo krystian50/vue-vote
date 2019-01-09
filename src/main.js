16d708c0aaeb21fcb85dda0a606d91017daebc68
Krystian Fras <krystian@fras.me>
2019-01-09 00:59:22 +0000

    dev-tools on prod fixed

--- src/main.js
+++ src/main.js
@@ -8,12 +8,13 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 Vue.config.productionTip = false;
-// add dev-tools on prod for devmeeting purpose
-// don't it in real production app
-Vue.config.devtools = true;
 
 new Vue({
   router,
   store,
   render: h => h(App),
 }).$mount('#app');
+
+// add dev-tools on prod for devmeeting purpose
+// don't it in real production app
+Vue.config.devtools = true;
